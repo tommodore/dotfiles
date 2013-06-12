b0c0ed21aeabde7cf2bb6a461a96d77a5bfe986d
Thomas Kräuter <kraeutert@gmail.com>
2013-06-12 18:56:50 +0300

    Update makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc .zlogin"    # list of files/folders to symlink in homedir
+files="gvimrc vimrc vim zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
