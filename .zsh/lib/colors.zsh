3415c019fac07f1cb03bcedcaee76c4f0b719588
tommodore <kraeutert@gmail.com>
2013-06-12 17:21:38 +0200

    changed colors in zsh

--- .zsh/lib/colors.zsh
+++ .zsh/lib/colors.zsh
@@ -1,7 +1,7 @@
 typeset -Ag FX FG BG
 
-LSCOLORS="exfxcxdxbxegedabagacad"
-LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+#LSCOLORS="exfxcxdxbxegedabagacad"
+#LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
 
 FX=(
     reset     "%{[00m%}"
