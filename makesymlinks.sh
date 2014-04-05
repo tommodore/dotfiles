a66b114405c344a1389ddc0fb8ff370f8a41c5fa
Thomas Kräuter <kraeutert@gmail.com>
2014-04-05 16:38:17 +0200

    added tmux config

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,8 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc zlogin zshrc vimrc vim gitconfig tmux.conf"    # list of files/folders to symlink in homedir
+# list of files/folders to symlink in homedir
+files="bashrc zlogin zshrc vimrc vim gitconfig tmux.conf gemrc"
 
 ##########
 
