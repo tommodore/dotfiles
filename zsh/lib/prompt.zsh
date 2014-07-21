640ec445232413997a4d6a483eebccc0c84281c3
Thomas Kräuter <kraeutert@gmail.com>
2014-07-21 21:17:15 +0200

    changed git prompt

--- zsh/lib/prompt.zsh
+++ zsh/lib/prompt.zsh
@@ -2,13 +2,14 @@ setopt prompt_subst
 autoload -U promptinit && promptinit
 
 
-ZSH_THEME_GIT_PROMPT_NOCACHE=1
+# ZSH_THEME_GIT_PROMPT_NOCACHE=1
 
 # left
 #PROMPT='%{$FG[226]%}%(!.#.⚡)%{$reset_color%} '
-PROMPT='%{$FG[007]%}%n@%m%{$FG[015]%}: %{$FG[003]%}%30<...<%~%<<%u%{$reset_color%}   $(git_super_status)
-%{$FG[007]%}%D{%H:%M}%{$reset_color%} $ '
+# PROMPT='%{$FG[007]%}%n@%m%{$FG[015]%}: %{$FG[003]%}%30<...<%c%<<%u%{$reset_color%}   $(git_super_status)
+# %{$FG[007]%}%D{%H:%M}%{$reset_color%} $ '
 
 # right
 #RPROMPT='%{$fg_bold[blue]%}%m%{$fg_bold[white]%}:%{$fg_bold[cyan]%}%30<...<%~%<<%u%{$reset_color%}$(git_super_status)'
 
+prompt pure
