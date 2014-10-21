536dc4d74db8ae212125016f9d7779e3489df011
Thomas Kräuter <kraeutert@gmail.com>
2014-10-21 17:37:24 +0200

    fix makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
@@ -38,3 +37,4 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/.$file ~/.$file
 done
+
