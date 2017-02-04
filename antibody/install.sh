9fcaaa6591722c591f6a18ce3a7d2015b7dbdc25
Thomas Kräuter <tommodore@mbp.fritz.box>
2017-02-04 14:48:06 +0100

    update install script

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if which brew >/dev/null 2>&1; then
-  brew untap -q getantibody/homebrew-antibody || true
-  brew tap -q getantibody/homebrew-antibody
+  brew untap -q  getantibody/tap/antibody || true
+  brew tap -q getantibody/tap/antibody 
   brew install getantibody/tap/antibody
 else
   curl -sL https://git.io/antibody | sh -s
